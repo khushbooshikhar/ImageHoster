e93ad8fe7a618963980b035ffc3a002cd0e1530f
khushboo <juhi.khushboo.89@gmail.com>
2020-09-21 01:09:33 +0530

    updated comment

--- src/main/java/ImageHoster/service/CommentService.java
+++ src/main/java/ImageHoster/service/CommentService.java
@@ -14,12 +14,14 @@ public class CommentService {
 	@Autowired
 	private CommentRepository commentRepository;
 	
-/** The method calls the createComment() method in the Repository and passes the comment to be persisted in the database */
+/** The method calls the createComment() method in the Repository
+ * and passes the comment to be persisted in the database */
 	public Comment createComment(Comment comment) {
 		return commentRepository.createComment(comment);
 	}
 	
-/** Call the getAllComments() method in the Repository and obtain a List of all the Comments in the database based on image id */
+/** Call the getAllComments() method in the Repository and obtain a
+ * List of all the Comments in the database based on image id */
 	public List<Comment> getAllComments(Integer imageId, String imageTitle) {
 		return commentRepository.getAllComments(imageId, imageTitle);
     }
